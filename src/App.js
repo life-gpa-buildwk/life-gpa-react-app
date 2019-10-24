cb608ee0359574f2426b8671817fdf2254c3d63f
kiamco <kiamcoo@gmail.com>
2019-10-24 13:28:40 -0700

    somethin

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import LoginNav from "./components/Navs/loginNav";
 import Signup from "./components/signupFrom"
 import Dashboard from "./components/dashboard";
 import GetStarted from "./components/letsGetStarted";
-import PrivateRoute from "./components/Auth/privateRoute"
+import { BrowserRouter } from 'react-router-dom';
 
 import './styles/index.scss';
 import "./App.css";
@@ -13,16 +13,15 @@ import "./App.css";
 function App(props) {
   return (
     <div className="App">
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/signup'component={Signup} />
-        <Route exact path='/dash' component={Dashboard} />
-        <Route exact path='/getStarted' component={GetStarted} />
-        P
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/signup' component={Signup} />
+      <Route exact path='/dash' component={Dashboard} />
+      <Route excat path='/getStarted' component={GetStarted} />
+
       {/* insert PrivateRoute  here*/}
-        {/* <PrivateRoute exact path='/dashboard/:id' component={Dashboard} /> */}
-        <footer>
-          <p> <span>LifeGPA 2019</span> | Privacy | Terms & Conditions </p>
-        </footer>
+      <footer>
+        <p> <span>LifeGPA 2019</span> | Privacy | Terms & Conditions </p>
+      </footer>
     </div>
   );
 }
